28e5e3583997b4c8eacaba7ee7db3c6b888c96bf
EzeCB <151409607+EzeCB@users.noreply.github.com>
2024-05-09 19:18:11 -0300

    Actualizacion

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <title>Inicio de sesión</title>
+   <link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 
 <body>
